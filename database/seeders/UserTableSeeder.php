5bd6122ffd2a16091cda5b78bca270f53b668882
salman608 <salman608bd@gmail.com>
2023-12-23 12:22:09 +0600

    setup laravel and breeze

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            // For Admin
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'admin',
+                'status' => '1',
+            ],
+
+            // For Instructor
+            [
+                'name' => 'Instructor',
+                'username' => 'instructor',
+                'email' => 'instructor@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'instructor',
+                'status' => '1',
+            ],
+
+            // For user
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'user',
+                'status' => '1',
+            ],
+
+            // For user
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@lms.com',
+                'password' => Hash::make('111'),
+                'role' => 'user',
+                'status' => '1',
+            ],
+        ]);
+    }
+}
