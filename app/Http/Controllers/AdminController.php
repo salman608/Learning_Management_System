ebd6ba55911541101cb90e5c6070f153559bae67
salman608 <salman608bd@gmail.com>
2023-12-24 14:39:45 +0600

    admin paswword updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -61,5 +62,41 @@ class AdminController extends Controller
             'alert-type' => "success"
         );
         return redirect()->back()->with($notification);
+    } //end
+
+    public function AdminChangePassword()
+    {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_change_password', compact('profileData'));
     }
+
+    public function AdminPasswordUpdate(Request $request)
+    {
+        //validation
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required | confirmed',
+
+        ]);
+
+        if (!Hash::check($request->old_password, auth::user()->password)) {
+            $notification = array(
+                'message' => "Old Password Does not Match!",
+                'alert-type' => "error"
+            );
+            return redirect()->back()->with($notification);
+        }
+
+        //Update New Password
+        User::whereId(auth::user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+
+        $notification = array(
+            'message' => "Password Update Successfully!",
+            'alert-type' => "success"
+        );
+        return redirect()->back()->with($notification);
+    } //end
 }
--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -393,8 +393,9 @@
                     <li><a class="dropdown-item d-flex align-items-center" href="{{ route('admin.profile') }}"><i
                                 class="bx bx-user fs-5"></i><span>Profile</span></a>
                     </li>
-                    <li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
-                                class="bx bx-cog fs-5"></i><span>Settings</span></a>
+                    <li><a class="dropdown-item d-flex align-items-center"
+                            href="{{ route('admin.change.password') }}"><i class="bx bx-cog fs-5"></i><span>Change
+                                Password</span></a>
                     </li>
                     <li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
                                 class="bx bx-home-circle fs-5"></i><span>Dashboard</span></a>
