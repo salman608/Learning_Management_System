0529e306fe2fe57d186521a128ed8385822a268b
salman608 <salman608bd@gmail.com>
2023-12-26 12:15:13 +0600

    frontend user dashboard profile edit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function Index()
     {
         return view('frontend.index');
+    }
+
+    public function UserProfile()
+    {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('frontend.dashboard.edit_profile', compact('profileData'));
     }
 }
