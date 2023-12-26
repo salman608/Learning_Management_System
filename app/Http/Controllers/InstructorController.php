3c94af81752e3dc94492b8b9f0fcb46f0d8bb9a3
salman608 <salman608bd@gmail.com>
2023-12-26 09:56:41 +0600

    instructor profile updated

--- app/Http/Controllers/InstructorController.php
+++ app/Http/Controllers/InstructorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -27,4 +28,37 @@ class InstructorController extends Controller
     {
         return view('instructor.instructor_login');
     }
+
+    public function InstructorProfile()
+    {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('instructor.instructor_profile_view', compact('profileData'));
+    } //end
+
+    public function InstructorProfileStore(Request $request)
+    {
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->username = $request->username;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            @unlink(public_path('upload/instructor_images/' . $data->photo));
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('upload/instructor_images'), $filename);
+            $data['photo'] = $filename;
+        }
+
+        $data->save();
+        $notification = array(
+            'message' => "Instructor Profile Updated Successful",
+            'alert-type' => "success"
+        );
+        return redirect()->back()->with($notification);
+    } //end
 }
--- routes/web.php
+++ routes/web.php
@@ -50,5 +50,7 @@ Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.
 Route::middleware(['auth', 'roles:instructor'])->group(function () {
     Route::get('/instructor/dashboard', [InstructorController::class, 'InstructorDashboard'])->name('instructor.dashboard');
     Route::get('/instructor/logout', [InstructorController::class, 'InstructorLogout'])->name('instructor.logout');
+    Route::get('/instructor/profile', [InstructorController::class, 'InstructorProfile'])->name('instructor.profile');
+    Route::post('/instructor/profile/store', [InstructorController::class, 'InstructorProfileStore'])->name('instructor.profile.store');
 }); //end admin middleware
 Route::get('/instructor/login', [InstructorController::class, 'InstructorLogin'])->name('instructor.login');
