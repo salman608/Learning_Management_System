992b19c73eb739b145894b92264c27b8b9afc780
salman608 <salman608bd@gmail.com>
2023-12-27 17:29:18 +0600

    instructor setup active menu

--- resources/views/instructor/body/sidebar.blade.php
+++ resources/views/instructor/body/sidebar.blade.php
@@ -1,3 +1,14 @@
+@php
+    $id = Auth::user()->id;
+    $instructorId = App\Models\User::find($id);
+
+    $status = $instructorId->status;
+@endphp
+
+
+
+
+
 <div class="sidebar-wrapper" data-simplebar="true">
     <div class="sidebar-header">
         <div>
@@ -20,86 +31,89 @@
             </a>
         </li>
 
+        @if ($status === '1')
+            <li class="menu-label">UI Elements</li>
 
-        <li class="menu-label">UI Elements</li>
+            <li>
+                <a href="javascript:;" class="has-arrow">
+                    <div class="parent-icon"><i class='bx bx-cart'></i>
+                    </div>
+                    <div class="menu-title">eCommerce</div>
+                </a>
+                <ul>
+                    <li> <a href="ecommerce-products.html"><i class='bx bx-radio-circle'></i>Products</a>
+                    </li>
+                    <li> <a href="ecommerce-products-details.html"><i class='bx bx-radio-circle'></i>Product
+                            Details</a>
+                    </li>
+                    <li> <a href="ecommerce-add-new-products.html"><i class='bx bx-radio-circle'></i>Add New
+                            Products</a>
+                    </li>
+                    <li> <a href="ecommerce-orders.html"><i class='bx bx-radio-circle'></i>Orders</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a class="has-arrow" href="javascript:;">
+                    <div class="parent-icon"><i class='bx bx-bookmark-heart'></i>
+                    </div>
+                    <div class="menu-title">Components</div>
+                </a>
+                <ul>
+                    <li> <a href="component-alerts.html"><i class='bx bx-radio-circle'></i>Alerts</a>
+                    </li>
+                    <li> <a href="component-accordions.html"><i class='bx bx-radio-circle'></i>Accordions</a>
+                    </li>
 
-        <li>
-            <a href="javascript:;" class="has-arrow">
-                <div class="parent-icon"><i class='bx bx-cart'></i>
-                </div>
-                <div class="menu-title">eCommerce</div>
-            </a>
-            <ul>
-                <li> <a href="ecommerce-products.html"><i class='bx bx-radio-circle'></i>Products</a>
-                </li>
-                <li> <a href="ecommerce-products-details.html"><i class='bx bx-radio-circle'></i>Product
-                        Details</a>
-                </li>
-                <li> <a href="ecommerce-add-new-products.html"><i class='bx bx-radio-circle'></i>Add New
-                        Products</a>
-                </li>
-                <li> <a href="ecommerce-orders.html"><i class='bx bx-radio-circle'></i>Orders</a>
-                </li>
-            </ul>
-        </li>
-        <li>
-            <a class="has-arrow" href="javascript:;">
-                <div class="parent-icon"><i class='bx bx-bookmark-heart'></i>
-                </div>
-                <div class="menu-title">Components</div>
-            </a>
-            <ul>
-                <li> <a href="component-alerts.html"><i class='bx bx-radio-circle'></i>Alerts</a>
-                </li>
-                <li> <a href="component-accordions.html"><i class='bx bx-radio-circle'></i>Accordions</a>
-                </li>
+                </ul>
+            </li>
+            <li>
+                <a class="has-arrow" href="javascript:;">
+                    <div class="parent-icon"><i class="bx bx-repeat"></i>
+                    </div>
+                    <div class="menu-title">Content</div>
+                </a>
+                <ul>
+                    <li> <a href="content-grid-system.html"><i class='bx bx-radio-circle'></i>Grid System</a>
+                    </li>
+                    <li> <a href="content-typography.html"><i class='bx bx-radio-circle'></i>Typography</a>
+                    </li>
 
-            </ul>
-        </li>
-        <li>
-            <a class="has-arrow" href="javascript:;">
-                <div class="parent-icon"><i class="bx bx-repeat"></i>
-                </div>
-                <div class="menu-title">Content</div>
-            </a>
-            <ul>
-                <li> <a href="content-grid-system.html"><i class='bx bx-radio-circle'></i>Grid System</a>
-                </li>
-                <li> <a href="content-typography.html"><i class='bx bx-radio-circle'></i>Typography</a>
-                </li>
+                </ul>
+            </li>
 
-            </ul>
-        </li>
+            <li>
+                <a href="user-profile.html">
+                    <div class="parent-icon"><i class="bx bx-user-circle"></i>
+                    </div>
+                    <div class="menu-title">User Profile</div>
+                </a>
+            </li>
 
-        <li>
-            <a href="user-profile.html">
-                <div class="parent-icon"><i class="bx bx-user-circle"></i>
-                </div>
-                <div class="menu-title">User Profile</div>
-            </a>
-        </li>
+            <li>
+                <a class="has-arrow" href="javascript:;">
+                    <div class="parent-icon"><i class="bx bx-error"></i>
+                    </div>
+                    <div class="menu-title">Errors</div>
+                </a>
+                <ul>
+                    <li> <a href="errors-404-error.html" target="_blank"><i class='bx bx-radio-circle'></i>404
+                            Error</a>
+                    </li>
+                    <li> <a href="errors-500-error.html" target="_blank"><i class='bx bx-radio-circle'></i>500
+                            Error</a>
+                    </li>
+                    <li> <a href="errors-coming-soon.html" target="_blank"><i class='bx bx-radio-circle'></i>Coming
+                            Soon</a>
+                    </li>
+                    <li> <a href="error-blank-page.html" target="_blank"><i class='bx bx-radio-circle'></i>Blank
+                            Page</a>
+                    </li>
+                </ul>
+            </li>
+        @else
+        @endif
 
-        <li>
-            <a class="has-arrow" href="javascript:;">
-                <div class="parent-icon"><i class="bx bx-error"></i>
-                </div>
-                <div class="menu-title">Errors</div>
-            </a>
-            <ul>
-                <li> <a href="errors-404-error.html" target="_blank"><i class='bx bx-radio-circle'></i>404
-                        Error</a>
-                </li>
-                <li> <a href="errors-500-error.html" target="_blank"><i class='bx bx-radio-circle'></i>500
-                        Error</a>
-                </li>
-                <li> <a href="errors-coming-soon.html" target="_blank"><i class='bx bx-radio-circle'></i>Coming
-                        Soon</a>
-                </li>
-                <li> <a href="error-blank-page.html" target="_blank"><i class='bx bx-radio-circle'></i>Blank
-                        Page</a>
-                </li>
-            </ul>
-        </li>
 
 
 
--- resources/views/instructor/index.blade.php
+++ resources/views/instructor/index.blade.php
@@ -1,6 +1,19 @@
 @extends('instructor.instructor_dashboard')
 @section('instructor')
+    @php
+        $id = Auth::user()->id;
+        $instructorId = App\Models\User::find($id);
+
+        $status = $instructorId->status;
+    @endphp
     <div class="page-content">
+
+        @if ($status === '1')
+            <h4>Instructor Account is <span class="text-success">Active</span> </h4>
+        @else
+            <h4>Instructor Account is <span class="text-danger">In Active</span> </h4>
+            <p class="text-warning"><b>Please wait admin will Check and approve your account</b></p>
+        @endif
         <div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
             <div class="col">
                 <div class="card radius-10 border-start border-0 border-4 border-info">
