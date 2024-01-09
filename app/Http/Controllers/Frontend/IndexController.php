7e1d5bead466129b9c49666901dd328134eb5abd
salman608 <salman608bd@gmail.com>
2024-01-09 11:05:12 +0600

    frontend course details goal requirment description

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Course;
+use App\Models\Course_goal;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -11,6 +12,7 @@ class IndexController extends Controller
     public function CourseDetails($id, $slug)
     {
         $course = Course::find($id);
-        return view('frontend.course.course_details', compact('course'));
+        $goals = Course_goal::where('course_id', $id)->orderBy('id', 'DESC')->get();
+        return view('frontend.course.course_details', compact('course', 'goals'));
     }
 }
--- resources/views/frontend/course/course_details.blade.php
+++ resources/views/frontend/course/course_details.blade.php
@@ -1,8 +1,8 @@
 @extends('frontend.master')
 @section('home')
     <!-- ================================
-                                                                    START BREADCRUMB AREA
-                                                                ================================= -->
+                                                                                    START BREADCRUMB AREA
+                                                                                ================================= -->
     <section class="breadcrumb-area pt-50px pb-50px bg-white pattern-bg">
         <div class="container">
             <div class="col-lg-8 mr-auto">
@@ -85,12 +85,12 @@
         </div><!-- end container -->
     </section><!-- end breadcrumb-area -->
     <!-- ================================
-                                                                    END BREADCRUMB AREA
-                                                                ================================= -->
+                                                                                    END BREADCRUMB AREA
+                                                                                ================================= -->
 
     <!--======================================
-                                                                        START COURSE DETAILS AREA
-                                                                ======================================-->
+                                                                                        START COURSE DETAILS AREA
+                                                                                ======================================-->
     <section class="course-details-area pb-20px">
         <div class="container">
             <div class="row">
@@ -99,19 +99,10 @@
                         <div class="course-overview-card bg-gray p-4 rounded">
                             <h3 class="fs-24 font-weight-semi-bold pb-3">What you'll learn?</h3>
                             <ul class="generic-list-item overview-list-item">
-                                <li><i class="la la-check mr-1 text-black"></i> Learn the core Java skills needed to apply
-                                    for Java developer positions in just 14 hours.</li>
-                                <li><i class="la la-check mr-1 text-black"></i> Be able to demonstrate your understanding of
-                                    Java to future employers.</li>
-                                <li><i class="la la-check mr-1 text-black"></i> Acquire essential java basics for
-                                    transitioning to the Spring Framework, Java EE, Android development and more.</li>
-                                <li><i class="la la-check mr-1 text-black"></i> Be able to sit for and pass the Oracle Java
-                                    Certificate exam if you choose.</li>
-                                <li><i class="la la-check mr-1 text-black"></i> Learn industry "best practices" in Java
-                                    software development from a professional Java developer who has worked in the language
-                                    for 18 years.</li>
-                                <li><i class="la la-check mr-1 text-black"></i> Obtain proficiency in Java 8 and Java 11.
-                                </li>
+                                @foreach ($goals as $goal)
+                                    <li><i class="la la-check mr-1 text-black"></i>{{ $goal->goal_name }}</li>
+                                @endforeach
+
                             </ul>
                         </div><!-- end course-overview-card -->
                         <div class="course-overview-card bg-gray p-4 rounded">
@@ -121,52 +112,33 @@
                         <div class="course-overview-card">
                             <h3 class="fs-24 font-weight-semi-bold pb-3">Requirements</h3>
                             <ul class="generic-list-item generic-list-item-bullet fs-15">
-                                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
-                                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
-                                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+                                <li>{{ $course->prerequisites }}</li>
+
                             </ul>
                         </div><!-- end course-overview-card -->
                         <div class="course-overview-card border border-gray p-4 rounded">
                             <h3 class="fs-20 font-weight-semi-bold">Top companies trust Aduca</h3>
                             <p class="fs-15 pb-1">Get your team access to Aduca's top 5,000+ courses</p>
                             <div class="pb-3">
-                                <img width="85" class="mr-3" src="images/sponsor-img.png" alt="company logo">
-                                <img width="80" class="mr-3" src="images/sponsor-img2.png" alt="company logo">
-                                <img width="80" class="mr-3" src="images/sponsor-img3.png" alt="company logo">
-                                <img width="70" class="mr-3" src="images/sponsor-img4.png" alt="company logo">
+                                <img width="85" class="mr-3" src="{{ asset('frontend/images/sponsor-img.png') }}"
+                                    alt="company logo">
+                                <img width="80" class="mr-3" src="{{ asset('frontend/images/sponsor-img2.png') }}"
+                                    alt="company logo">
+                                <img width="80" class="mr-3" src="{{ asset('frontend/images/sponsor-img3.png') }}"
+                                    alt="company logo">
+                                <img width="70" class="mr-3" src="{{ asset('frontend/images/sponsor-img4.png') }}"
+                                    alt="company logo">
                             </div>
                             <a href="for-business.html" class="btn theme-btn theme-btn-sm">Try Aduca for Business</a>
                         </div><!-- end course-overview-card -->
                         <div class="course-overview-card">
                             <h3 class="fs-24 font-weight-semi-bold pb-3">Description</h3>
-                            <p class="fs-15 pb-2">Lorem Ipsum has been the industry’s standard dummy text ever since the
-                                1500s, when an unknown printer took a galley of type and scrambled it to make a type
-                                specimen book. It has survived not only five centuries, Lorem Ipsum has been the industry’s
-                                standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                                scrambled it to make a type specimen book.</p>
-                            <p class="fs-15 pb-2">It has survived not only five centuries, but also the leap into electronic
-                                typesetting, remaining essentially unchanged.Lorem Ipsum is simply dummy text of the
-                                printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy</p>
-                            <p class="fs-15 pb-1">- Lorem ipsum dolor sit amet, consectetur.</p>
-                            <p class="fs-15 pb-1">- Lorem ipsum dolor sit amet, consectetur.</p>
-                            <p class="fs-15 pb-1">- Lorem ipsum dolor sit amet, consectetur.</p>
-                            <p class="fs-15 pt-3 pb-2 lh-22"><strong class="font-weight-semi-bold text-black">Are you
-                                    aiming to get your first Java Programming job but struggling to find out what skills
-                                    employers want</strong> and which course will give you those skills?</p>
-                            <p class="fs-15 pb-2">This course is designed to give you the Java skills you need to get a job
-                                as a Java developer. By the end of the course, you will understand Java extremely well and
-                                be able to build your own Java apps and be productive as a software developer.</p>
+                            <p class="fs-15 pb-2">
+                                {!! $course->description !!}
+                            </p>
                             <div class="collapse" id="collapseMore">
-                                <p class="fs-15 pb-2">The core java material you need to learn java development is covered
-                                    in the first seven sections (around 14 hours in total). The Java Basics are covered in
-                                    those sections. The rest of the course covers intermediate, advanced, and optional
-                                    material you do not technically need to go through.</p>
-                                <h4 class="fs-20 font-weight-semi-bold py-2">Who this course is for:</h4>
-                                <ul class="generic-list-item generic-list-item-bullet fs-15">
-                                    <li>Anyone who wants to become a computer programmer</li>
-                                    <li>Anyone who wants to become a computer programmer</li>
-                                    <li>Anyone who wants to become a computer programmer</li>
-                                </ul>
+                                <p class="fs-15 pb-2">{{ $course->prerequisites }}</p>
+
                             </div>
                             <a class="collapse-btn collapse--btn fs-15" data-toggle="collapse" href="#collapseMore"
                                 role="button" aria-expanded="false" aria-controls="collapseMore">
@@ -972,12 +944,12 @@
         </div><!-- end container -->
     </section><!-- end course-details-area -->
     <!--======================================
-                                                                        END COURSE DETAILS AREA
-                                                                ======================================-->
+                                                                                        END COURSE DETAILS AREA
+                                                                                ======================================-->
 
     <!--======================================
-                                                                        START RELATED COURSE AREA
-                                                                ======================================-->
+                                                                                        START RELATED COURSE AREA
+                                                                                ======================================-->
     <section class="related-course-area bg-gray pt-60px pb-60px">
         <div class="container">
             <div class="related-course-wrap">
@@ -1121,12 +1093,12 @@
         </div><!-- end container -->
     </section><!-- end related-course-area -->
     <!--======================================
-                                                                        END RELATED COURSE AREA
-                                                                ======================================-->
+                                                                                        END RELATED COURSE AREA
+                                                                                ======================================-->
 
     <!--======================================
-                                                                        START CTA AREA
-                                                                ======================================-->
+                                                                                        START CTA AREA
+                                                                                ======================================-->
     <section class="cta-area pt-60px pb-60px position-relative overflow-hidden">
         <span class="stroke-shape stroke-shape-1"></span>
         <span class="stroke-shape stroke-shape-2"></span>
@@ -1171,8 +1143,8 @@
         </div><!-- end container -->
     </section><!-- end cta-area -->
     <!--======================================
-                                                                        END CTA AREA
-                                                                ======================================-->
+                                                                                        END CTA AREA
+                                                                                ======================================-->
 
     <!-- start scroll top -->
     <div id="scroll-top">
